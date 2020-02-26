ede011a9eb76dd7ee561792a29188799644ab4c1
ielemson <ielemson@gmail.com>
2020-02-26 16:58:35 +0100

    react express boiler plate

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/customers", (re, res) => {
+  const customers = [
+    {
+      id: 1,
+      firstname: "Elemmson",
+      lastname: "Ifeanyi"
+    },
+    {
+      id: 2,
+      firstname: "Ella",
+      lastname: "Onyii"
+    },
+    {
+      id: 3,
+      firstname: "Precious",
+      lastname: "Melah"
+    },
+    {
+      id: 4,
+      firstname: "Okon",
+      lastname: "Ernest"
+    },
+    {
+      id: 5,
+      firstname: "Francis",
+      lastname: "Obi"
+    }
+  ];
+  res.json(customers);
+});
+
+const port = 5500;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
