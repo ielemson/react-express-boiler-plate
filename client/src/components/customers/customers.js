6c5c561764de2ef15789312bcf8cd47227f4725b
ielemson <ielemson@gmail.com>
2020-02-26 17:15:34 +0100

    added bootstrap and customer page

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./customers.css";
+
+class Customers extends Component {
+  render(h) {
+    return (
+      <div className="container">
+        <h2>Customers</h2>
+      </div>
+    );
+  }
+}
+
+export default Customers;
